3a34704550edbf072cea408ecff8157b3598d140
Timofei Menshikov <57629925+TimofeiMenshikov@users.noreply.github.com>
2023-08-21 16:27:41 +0300

    Update quadratic_solver.cpp
    перенос объявлений в quad_solver.h

--- quadratic_solver.cpp
+++ quadratic_solver.cpp
@@ -2,8 +2,7 @@
 #include <math.h>
 #include <TXLib.h>
 
-#define MAXSTR 1000
-#define TEST
+#include "quad_solver.h"
 
 const double EPS = 0.0000001;
 
@@ -17,17 +16,6 @@ struct Coefficients
 };
 
 
-void test();
-struct Coefficients coef_input_from_file(struct Coefficients coefficients, FILE* inputfile);
-struct Coefficients coef_input(struct Coefficients coefficients);
-void print_solutions(double* solutions, int num_of_solutions);
-int solve_quadratic(struct Coefficients coefficients, double* solutions);
-int solve_linear(struct Coefficients coefficients, double* solutions);
-int solve_equation(struct Coefficients coefficients, double* solutions);
-void print_test(bool is_passed, int test_number, int num_of_solutions, double* solutions, int right_num_of_solutions, double* right_answers, struct Coefficients coefficients);
-bool check_answers(int num_of_solutions, int right_num_of_solutions, double* solutions, double* right_answers);
-bool check_num_of_solutions(int num_of_solutions, int right_num_of_solutions);
-bool check_solver(double *solutions, double *right_answers, int num_of_solutions, int right_num_of_solutions);
 int main()
 {
     #ifdef TEST
