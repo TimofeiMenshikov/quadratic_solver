c30291b869bb9e8fd752ee9ba32e206d4d8e3128
Timofei Menshikov <tim777men@gmail.com>
2023-08-23 19:15:57 +0300

    Update main.h
    new subpage

--- main.h
+++ main.h
@@ -3,7 +3,7 @@
  * This project helps user to sovle quadratic equatation.
 
  * - \subpage how_to_use "How to use it"
- *
+ * - \subpage how_to_enable_testmode "How to enable testmode"
  */
 
 /*! \page how_to_use How to use it?
@@ -15,4 +15,15 @@
  * the program will display the solutions and their number.
  */
 
+
+/*! \page how_to_enable_testmode How to enable testmode
+ * testmode
+ * write in command line:
+ *
+ * \code g++ main.cpp test.cpp -o main -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wno-missing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -Werror=vla -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D TEST \endcode
+ * testmode with input debug
+ * write in command line:
+ *
+ * \code g++ main.cpp test.cpp -o main -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wno-missing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -Werror=vla -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D TEST -D INPUT_DEBUG \endcode
+ */
 
