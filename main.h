132b8108f09be4b291173ce00c6f877dca327258
Timofei Menshikov <57629925+TimofeiMenshikov@users.noreply.github.com>
2023-08-23 17:38:00 +0300

    Create main.h
    main page doxygen

---
+++ main.h
@@ -0,0 +1,18 @@
+/*! \mainpage Quadratic solver
+ *
+ * This project helps user to sovle quadratic equatation.
+
+ * - \subpage how_to_use "How to use it"
+ *
+ */
+
+/*! \page how_to_use How to use it?
+ *
+ * write in command line: \code g++ main.cpp test.cpp -o main -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include- dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored- qualifiers -Wlogical-op -Wno-missing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype- limits -Wwrite-strings -Werror=vla -D_DEBUG -D_EJUDGE_CLIENT_SIDE \endcode
+ *
+ * enter 3 numbers - the coefficients a, b, c of the equation ax^2 + bx + c = 0 respectively. The program will notify you of the successful entry of each variable.
+ *
+ * the program will display the solutions and their number.
+ */
+
+
