c735a598fb230b19a673fd308068d44a80c12391
Timofei Menshikov <tim777men@gmail.com>
2023-08-24 17:45:48 +0300

    Update main.cpp
    assert -> if

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ main program with int main() function inside
 
 int main()
 {
+    #ifdef COLOR_COMANDLINE
+        remove_color_cmd();
+    #endif /* COLOR_COMANDLINE */
     #ifdef TEST
         test();
     #else /* TEST */
@@ -28,31 +31,45 @@ void release()
     //TODO tell me about NaN types
     coef_input(&coefficients);
 
-    assert(!isnan(coefficients.a));
-    assert(!isnan(coefficients.b));
-    assert(!isnan(coefficients.c));
+
+    assert(isnan(coefficients.a) or isnan(coefficients.b) or isnan(coefficients.c));
+
+
+    if (isnan(coefficients.a) or isnan(coefficients.b) or isnan(coefficients.c))
+    {
+        colored_print("line 35 main.cpp:\n", red);
+        colored_print("coefficients is not scaned\n", red);
+
+        return;
+    }
+
 
     solutions.number = solve_equation(&coefficients, solutions.arr);
 
-    assert(solutions.number != INVALID_NUMBER);
+    if (solutions.number == INVALID_NUMBER) 
+    {
+        colored_print("line 44: main.cpp: \n", red);
+        colored_print("problems with function solve_equation\n", red);
+        return;
+    }
 
     print_solutions(&solutions);
 }
 
 
-void clean_buffer()
+void clean_buffer()  ///clears the input stream until the end of line character
 {
     while (getchar() != '\n');
 }
 
 
-bool is_equal(double first_number, double second_number)
+bool is_equal(double first_number, double second_number) ///checks if two numbers are equal
 {
     return (fabs(first_number - second_number) < EPS);
 }
 
 
-int comparison_of_two_numbers(double first_number, double second_number)///comparison of two double numbers with using epsilon
+int comparison_of_two_numbers(double first_number, double second_number) ///comparison of two double numbers with using epsilon
 {
     if ((first_number - second_number) > EPS)
     {
@@ -67,23 +84,23 @@ int comparison_of_two_numbers(double first_number, double second_number)///compa
 }
 
 
-bool is_not_inputed(double* one_coef_pointer)
+bool is_not_inputed(double* one_coef_pointer) ///returns true if scanf did not work correctly
 {
     return scanf("%lf", one_coef_pointer) == 0;
 }
 
 
-void one_coef_input(double* one_coef_pointer)
+void one_coef_input(double* one_coef_pointer) ///a function for entering one coefficient with a check for the correctness of the input
 {
     while (is_not_inputed(one_coef_pointer))
     {
         clean_buffer();
 
-#ifdef INPUT_DEBUG
+        #ifdef INPUT_DEBUG
 
-        printf("buffer is cleaned\n");
+            printf("buffer is cleaned\n");
 
-#endif /* INPUT_DEBUG */
+        #endif /* INPUT_DEBUG */
 
     }
 }
@@ -107,7 +124,7 @@ void coef_input(struct Coefficients* coef_pointer)  /// introduces coefficients.
 }
 
 
-int solve_equation(struct Coefficients* coef_pointer, double solutions_array[])
+int solve_equation(struct Coefficients* coef_pointer, double solutions_array[]) /// solves equatation ax^2 + bx + c = 0
 {
 
     int num_of_solutions = INVALID_NUMBER;
@@ -125,7 +142,7 @@ int solve_equation(struct Coefficients* coef_pointer, double solutions_array[])
 }
 
 
-int solve_linear(struct Coefficients* coef_pointer, double solutions_array[])
+int solve_linear(struct Coefficients* coef_pointer, double solutions_array[]) /// solves equatation bx + c = 0
 {
     int num_of_solutions = INVALID_NUMBER;
 
@@ -133,6 +150,14 @@ int solve_linear(struct Coefficients* coef_pointer, double solutions_array[])
     {
         num_of_solutions = ONE_SOLUTION;
         double x = -coef_pointer->c / coef_pointer->b;
+
+        if (isinf(x))
+        {
+            colored_print("line 148 main.cpp:\n", red);
+            colored_print("division by zero\n", red);
+            return INVALID_NUMBER;
+        }
+
         solutions_array[0] = x;
     }
     else
@@ -151,7 +176,7 @@ int solve_linear(struct Coefficients* coef_pointer, double solutions_array[])
 }
 
 
-int solve_quadratic(struct Coefficients* coef_pointer, double solutions_array[])
+int solve_quadratic(struct Coefficients* coef_pointer, double solutions_array[]) ///solves equatation ax^2 + bx + c = 0 (a != 0)
 {
     int num_of_solutions = INVALID_NUMBER;
 
@@ -166,6 +191,13 @@ int solve_quadratic(struct Coefficients* coef_pointer, double solutions_array[])
         double x1 = (-coef_pointer->b + root_of_d) / (2 * coef_pointer->a);
         double x2 = (-coef_pointer->b - root_of_d) / (2 * coef_pointer->a);
 
+        if (isinf(x1) or isinf(x2))
+        {
+            colored_print("line 190 main.cpp:\n", red);
+            colored_print("division by zero", red);
+            return INVALID_NUMBER;
+        }
+
         solutions_array[0] = x1;
         solutions_array[1] = x2;
     }
@@ -179,6 +211,13 @@ int solve_quadratic(struct Coefficients* coef_pointer, double solutions_array[])
 
         double x = -coef_pointer->b / (2  * coef_pointer->a);
 
+        if (isinf(x))
+        {
+            colored_print("line 208 main.cpp:\n", red);
+            colored_print("division by zero\n", red);
+            return INVALID_NUMBER;
+        }
+
         solutions_array[0] = x;
     }
 
@@ -186,7 +225,7 @@ int solve_quadratic(struct Coefficients* coef_pointer, double solutions_array[])
 }
 
 
-void print_solutions(struct Solutions* solutions_pointer)   //displays solutions and their numbers
+void print_solutions(struct Solutions* solutions_pointer)   ///displays solutions and their numbers
 {
     if (solutions_pointer->number != INF_SOLUTIONS)
     {
@@ -206,7 +245,7 @@ void print_solutions(struct Solutions* solutions_pointer)   //displays solutions
 }
 
 
-void nulling_coefficients(struct Coefficients* coef_pointer)
+void nulling_coefficients(struct Coefficients* coef_pointer)  ///turns coefficients a, b, c into NAN
 {
     coef_pointer->a = NAN;
     coef_pointer->b = NAN;
@@ -214,7 +253,7 @@ void nulling_coefficients(struct Coefficients* coef_pointer)
 }
 
 
-void nulling_answers(struct Solutions* solutions_pointer)
+void nulling_answers(struct Solutions* solutions_pointer) ///turns solutions x1, x2 into NAN
 {
     (solutions_pointer->arr)[0] = NAN;
     (solutions_pointer->arr)[0] = NAN;
@@ -222,15 +261,29 @@ void nulling_answers(struct Solutions* solutions_pointer)
 }
 
 #ifdef COLOR_COMANDLINE
-void color_cmd(short unsigned int text_color)
-{
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), text_color);
-}
+    void color_cmd(short unsigned int text_color) ///the following text in the console will be of the specified color
+    {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), text_color);
+    }
 
 
-void remove_color_cmd()
-{
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),  0x0f); 
-}
+    void remove_color_cmd() /// the following text in the console will be white
+    {
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),  0x0f); 
+    }
+
+
 #endif /* COLOR_COMANDLINE */
+
+
+void colored_print(const char* text, short unsigned int text_color) /// can make text color, but does not work with variables
+{
+    #ifdef COLOR_COMANDLINE
+        color_cmd(text_color);
+    #endif /* COLOR_COMANDLINE */
+    printf("%s", text);
+    #ifdef COLOR_COMANDLINE
+        remove_color_cmd();
+    #endif /* COLOR_COMANDLINE */
+}
 
