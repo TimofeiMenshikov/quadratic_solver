9a61817f9c8a4b4b42ef45e02822c79a7ec1da12
Timofei Menshikov <tim777men@gmail.com>
2023-08-25 16:08:01 +0300

    add my_assert

--- quad_solver.h
+++ quad_solver.h
@@ -1,10 +1,14 @@
-#ifndef QUAD_SOLVER_H
-#define QUAD_SOLVER_H
-
 /*!
 \file
 header file with constants, function declarations, standard includes
 */
+
+
+#ifndef QUAD_SOLVER_H
+#define QUAD_SOLVER_H
+#define my_assert(bool_expression, ERROR_CODE, line, cpp_file_name) \
+    (bool_expression == 0 ? printf("line %d %s: %s \n", line, cpp_file_name, #ERROR_CODE) : 1) \
+    
 
 
 //#include <TXLib.h>
@@ -12,11 +16,10 @@ header file with constants, function declarations, standard includes
 #include <math.h>
 #include <assert.h>
 
-
 /// constant for comparison of two numbers
 const double EPS = 0.0000001;
 
-enum number_of_solutions
+enum number_of_solutions  /// defines solution number constants for the solutions.numbrer variable
 {
     INVALID_NUMBER = -2,
     INF_SOLUTIONS  = -1,
@@ -32,19 +35,27 @@ enum number_comparison    /// constants for comparison_of_two_numbers function
     FIRST_IS_BIGGER = 1
 };
 
+enum error_number /// in case of an error, the function outputs an error code
+{
+    ISNAN = 1,
+    FILE_IS_NOT_OPENED = 2,
+    FILE_IS_EMPTY = 3,
+    DIVISION_BY_ZERO = 4,
+};
+
 struct Coefficients  ///  coefficients of equation ax^2 + bx + c = 0
 {
-    double a = NAN;
-    double b = NAN;
-    double c = NAN;
+    double a = NAN;  ///< a * x * x
+    double b = NAN;  ///< b * x
+    double c = NAN;  ///< c
 };
 
 
 struct Solutions /// the structure includes an array of 2 elements - the maximum countable number of solutions to the quadratic equation and the number of solutions, which is equal to one of the number_of_solutions constants
 {
-    double arr[2] = {NAN, NAN};
+    double arr[2] = {NAN, NAN}; ///< This is array of equation solutions
 
-    int number = INVALID_NUMBER;
+    int number = INVALID_NUMBER; ///< This is number of equation solutions. Uses enum number_of_solutions in quad_solver.h
 };
 
 
@@ -53,6 +64,8 @@ void clean_buffer();
 void test();
 
 void release();
+
+void are_coefficients_nan(const struct Coefficients* coef_pointer);
 
 void nulling_answers(struct Solutions* solutions_pointer);
 void nulling_coefficients(struct Coefficients* coef_pointer);
